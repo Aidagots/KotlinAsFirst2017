84f9700b80933e714df5e25824cc421d7cdc6b66
Aidagots <gotovceva_1999@mail.ru>
2017-10-19 16:36:24 +0300

    les 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -69,7 +69,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad + min/60.0 + sec/3600.0)/180.0 * PI
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad + min / 60.0 + sec / 3600.0) / 180.0 * PI
 
 /**
  * Тривиальная
@@ -77,7 +77,7 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad + min/60.0 + se
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2-x1) + sqr( y2-y1))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2 - x1) + sqr(y2 - y1))
 
 /**
  * Простая
@@ -95,7 +95,7 @@ fun thirdDigit(number: Int): Int = ( number % 1000 ) / 100
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
 fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (hoursArrive * 60 -
-        hoursDepart * 60 )+ (minutesArrive - minutesDepart)
+        hoursDepart * 60) + (minutesArrive - minutesDepart)
 
 /**
  * Простая
@@ -114,4 +114,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (percent
  * Пользователь задает целое трехзначное число (например, 478).
  *Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = ((number / 100 + ( number % 10 * 100) + number / 10 % 10 * 10))
+fun numberRevert(number: Int): Int = ((number / 100 + (number % 10 * 100) + number / 10 % 10 * 10))
